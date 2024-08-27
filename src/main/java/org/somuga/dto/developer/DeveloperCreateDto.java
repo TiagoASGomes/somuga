b1ad08d8002548d77bafe8c6568ea96500135ba5
TiagoASGomes <150845545+TiagoASGomes@users.noreply.github.com>
2024-08-27 09:56:38 +0100

    Removed developer socials

--- src/main/java/org/somuga/dto/developer/DeveloperCreateDto.java
+++ src/main/java/org/somuga/dto/developer/DeveloperCreateDto.java
@@ -4,8 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-import java.util.List;
-
 import static org.somuga.util.message.Messages.INVALID_DEVELOPER_NAME;
 
 @Schema(description = "Developer create data transfer object")
@@ -13,8 +11,6 @@ public record DeveloperCreateDto(
         @NotBlank(message = INVALID_DEVELOPER_NAME)
         @Pattern(regexp = "^[a-zA-Z0-9 ]{1,255}$", message = INVALID_DEVELOPER_NAME)
         @Schema(description = "The developer name", example = "Mojang")
-        String developerName,
-        @Schema(description = "A list of social media links", example = "[\"https://twitter.com/mojang\",\"https://www.facebook.com/mojang\"]")
-        List<String> socials
+        String developerName
 ) {
 }
